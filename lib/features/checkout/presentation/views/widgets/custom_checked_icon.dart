c2521021d9e97eaffd1f1b131e96eec01f44a026
MohamedNasrr <mohamednasrrkh@gmail.com>
2024-10-21 18:35:58 +0300

    refactor thank you screen

---
+++ lib/features/checkout/presentation/views/widgets/custom_checked_icon.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomCheckedIcon extends StatelessWidget {
+  const CustomCheckedIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Positioned(
+      top: -50,
+      left: 0,
+      right: 0,
+      child: CircleAvatar(
+        radius: 50,
+        backgroundColor:  Color(0xffD9D9D9),
+        child: CircleAvatar(
+          radius: 40,
+          backgroundColor: Color(0xff34A853),
+          child: Icon(
+            Icons.check,
+            size: 50,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payments_app/features/checkout/presentation/views/widgets/custom_checked_icon.dart';
+import 'package:payments_app/features/checkout/presentation/views/widgets/custom_dashed_line.dart';
+import 'package:payments_app/features/checkout/presentation/views/widgets/thank_you_card.dart';
 
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
@@ -15,32 +18,12 @@ class ThankYouViewBody extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              color: const Color(0xffD9D9D9),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
+          const ThankYouCard(),
           Positioned(
             bottom: MediaQuery.sizeOf(context).height * 0.2 + 10,
             left: 20 + 3,
             right: 20 + 3,
-            child: Row(
-              children: List.generate(
-                  35,
-                  (index) => Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 1),
-                      child: Container(
-                        color: const Color(0xffB8B8B8),
-                        height: 2,
-                      ),
-                    ),
-                  ),
-              ),
-            ),
+            child: const CustomDashedLine(),
           ),
           Positioned(
             left: -20,
@@ -56,26 +39,10 @@ class ThankYouViewBody extends StatelessWidget {
               backgroundColor: Colors.white,
             ),
           ),
-          const Positioned(
-            top: -50,
-            left: 0,
-            right: 0,
-            child: CircleAvatar(
-              radius: 50,
-              backgroundColor:  Color(0xffD9D9D9),
-              child: CircleAvatar(
-                radius: 40,
-                backgroundColor: Color(0xff34A853),
-                child: Icon(
-                  Icons.check,
-                  size: 50,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
+          const CustomCheckedIcon(),
         ],
       ),
     );
   }
 }
+
